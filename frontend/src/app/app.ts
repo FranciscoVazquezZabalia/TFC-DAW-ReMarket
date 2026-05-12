ddfd33dbbaef38b643fd9bd39653727f6369e504
Francisco Vazquez <fvz2001@icloud.com>
2026-05-12 20:46:13 +0200

    feat(frontend): maquetacion de login y registro estilo minimalista

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -10,31 +10,4 @@ import { CommonModule } from '@angular/common';
 })
 export class App {
   protected readonly title = signal('frontend');
-
-showcaseSections = [
-    { 
-      title: 'Mundo Apple', 
-      subtitle: 'El ecosistema perfecto. Como nuevo.', 
-      img: 'https://images.unsplash.com/photo-1491933382434-500287f9b54b?q=80&w=2000&auto=format&fit=crop',
-      textColor: 'text-dark'
-    },
-    { 
-      title: 'Tecnología', 
-      subtitle: 'Potencia para tu día a día.', 
-      img: 'https://images.unsplash.com/photo-1498049794561-7780e7231661?q=80&w=2000&auto=format&fit=crop',
-      textColor: 'text-white'
-    },
-    { 
-      title: 'Moda', 
-      subtitle: 'Renueva tu estilo. Cuida el planeta.', 
-      img: 'https://images.unsplash.com/photo-1445205170230-053b83016050?q=80&w=2000&auto=format&fit=crop',
-      textColor: 'text-white'
-    },
-    { 
-      title: 'Hogar', 
-      subtitle: 'Espacios que inspiran historia.', 
-      img: 'https://images.unsplash.com/photo-1513694203232-719a280e022f?q=80&w=2000&auto=format&fit=crop',
-      textColor: 'text-dark'
-    }
-  ];
 }
---
+++ frontend/src/app/pages/login/login.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class Login {}
